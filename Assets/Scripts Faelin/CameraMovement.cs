c555ebd062dc85f12d5c7d07f62761e7c6c2986c
Qwerzer23 <127936920+JaredGrdsSL@users.noreply.github.com>
2023-12-13 11:57:13 +0100

    Started energy bar and added fixed width

--- Assets/Scripts Faelin/CameraMovement.cs
+++ Assets/Scripts Faelin/CameraMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
@@ -8,21 +9,31 @@ public class CameraMovement : MonoBehaviour {
 
     private PlayerMovement playerMovement;
 
-    void Start() {
+    public Cinemachine.CinemachineVirtualCamera virtualCam;
+    public SpriteRenderer wallSprite;
+
+
+
+    void Start()
+    {
         playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+
+        virtualCam.m_Lens.OrthographicSize = wallSprite.bounds.size.x * Screen.height / Screen.width * 0.5f;
     }
 
     // Update is called once per frame
-    void Update() {
-        if (!playerMovement.isBat) {
+    void Update()
+    {
+
+        if(!playerMovement.isBat) {
             UpdateCamera(playerCameraOffset);
-        }
-        else if (playerMovement.gameObject.transform.position.y > transform.position.y - batCameraOffset){
+        } else if(playerMovement.gameObject.transform.position.y > transform.position.y - batCameraOffset) {
             UpdateCamera(batCameraOffset);
         }
     }
 
-    private void UpdateCamera(float offset) {
+    private void UpdateCamera(float offset)
+    {
         transform.position = new Vector3(transform.position.x, playerMovement.gameObject.transform.position.y + offset, transform.position.z);
     }
 }
--- Assets/Scripts Jared/Respawn.cs
+++ Assets/Scripts Jared/Respawn.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,9 +10,20 @@ public class Respawn : MonoBehaviour {
 
     public GameObject respawnPoint;
     public GameObject objectToRespawn;
+
+    public string myVariable;
     public string deadlyTag;
 
     public float respawnTime;
+
+    /// <summary>
+    /// MAKE A BOOLEAN FOR PLAYER OR NOT
+    /// </summary>
+
+    private void Awake()
+    {
+        objectToRespawn.transform.position = respawnPoint.transform.position;
+    }
 
     IEnumerator RespawnWait(Collision2D other)
     {
